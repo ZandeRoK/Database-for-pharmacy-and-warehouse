99dc0446dfb63cf021ca0c58fa0ba55f30a72d2c
Vadym Kundilovskyi <vadimkmua9@gmail.com>
2018-09-20 17:04:49 +0200

    Add files via upload

---
+++ DataBaseTest/DataBaseTest/FormsForPharmacy/CreateOrder.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DataBaseTest.FormsForPharmacy
+{
+    partial class CreateOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDrugDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.warehouseBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet = new DataBaseTest._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet();
+            this.button1 = new System.Windows.Forms.Button();
+            this.warehouseBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dBMainDataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.warehouseTableAdapter1 = new DataBaseTest._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSetTableAdapters.WarehouseTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBMainDataSet1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDrugDataGridViewTextBoxColumn,
+            this.countDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.Column1});
+            this.dataGridView1.DataSource = this.warehouseBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(545, 461);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "№ id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDrugDataGridViewTextBoxColumn
+            // 
+            this.nameDrugDataGridViewTextBoxColumn.DataPropertyName = "NameDrug";
+            this.nameDrugDataGridViewTextBoxColumn.HeaderText = "Name Drug";
+            this.nameDrugDataGridViewTextBoxColumn.Name = "nameDrugDataGridViewTextBoxColumn";
+            // 
+            // countDataGridViewTextBoxColumn
+            // 
+            this.countDataGridViewTextBoxColumn.DataPropertyName = "Count";
+            this.countDataGridViewTextBoxColumn.HeaderText = "Available quantity";
+            this.countDataGridViewTextBoxColumn.Name = "countDataGridViewTextBoxColumn";
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Need quantity";
+            this.Column1.Name = "Column1";
+            // 
+            // warehouseBindingSource1
+            // 
+            this.warehouseBindingSource1.DataMember = "Warehouse";
+            this.warehouseBindingSource1.DataSource = this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet;
+            // 
+            // _C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet
+            // 
+            this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet.DataSetName = "_C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_D" +
+    "ATABASE_DB_MAIN_MDFDataSet";
+            this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 479);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // warehouseTableAdapter1
+            // 
+            this.warehouseTableAdapter1.ClearBeforeFill = true;
+            // 
+            // CreateOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 524);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "CreateOrder";
+            this.Text = "CreateOrder";
+            this.Load += new System.EventHandler(this.CreateOrder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBMainDataSet1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource warehouseBindingSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDrugDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource dBMainDataSet1BindingSource;
+        private _C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet _C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet;
+        private System.Windows.Forms.BindingSource warehouseBindingSource1;
+        private _C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSetTableAdapters.WarehouseTableAdapter warehouseTableAdapter1;
+    }
+}
---
+++ DataBaseTest/DataBaseTest/FormsForWarehouse/WarehouseForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace DataBaseTest.FormsForWarehouse
+{
+    partial class WarehouseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDrugDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.warehouseBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet = new DataBaseTest._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.warehouseTableAdapter1 = new DataBaseTest._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSetTableAdapters.WarehouseTableAdapter();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(551, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(498, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Orders wait for You here -->";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDrugDataGridViewTextBoxColumn,
+            this.countDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.warehouseBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(444, 394);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDrugDataGridViewTextBoxColumn
+            // 
+            this.nameDrugDataGridViewTextBoxColumn.DataPropertyName = "NameDrug";
+            this.nameDrugDataGridViewTextBoxColumn.HeaderText = "NameDrug";
+            this.nameDrugDataGridViewTextBoxColumn.Name = "nameDrugDataGridViewTextBoxColumn";
+            this.nameDrugDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // countDataGridViewTextBoxColumn
+            // 
+            this.countDataGridViewTextBoxColumn.DataPropertyName = "Count";
+            this.countDataGridViewTextBoxColumn.HeaderText = "Count";
+            this.countDataGridViewTextBoxColumn.Name = "countDataGridViewTextBoxColumn";
+            this.countDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // warehouseBindingSource1
+            // 
+            this.warehouseBindingSource1.DataMember = "Warehouse";
+            this.warehouseBindingSource1.DataSource = this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet;
+            // 
+            // _C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet
+            // 
+            this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet.DataSetName = "_C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_D" +
+    "ATABASE_DB_MAIN_MDFDataSet";
+            this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Available drugs in stock";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(996, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Complete order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(672, 28);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(444, 394);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(669, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Available orders to execute";
+            // 
+            // warehouseTableAdapter1
+            // 
+            this.warehouseTableAdapter1.ClearBeforeFill = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(870, 428);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 32);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(356, 429);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Edit mode";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(416, 428);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(40, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "On";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // WarehouseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 503);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "WarehouseForm";
+            this.Text = "Warehouse form";
+            this.Load += new System.EventHandler(this.WarehouseForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDrugDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label4;
+        private _C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet _C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSet;
+        private System.Windows.Forms.BindingSource warehouseBindingSource1;
+        private _C__USERS_ВАДИМ_DOCUMENTS_VISUAL_STUDIO_2015_PROJECTS_DATABASETEST_DATABASETEST_DATABASE_DB_MAIN_MDFDataSetTableAdapters.WarehouseTableAdapter warehouseTableAdapter1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
